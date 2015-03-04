c21ea6ddd844db1527cba5ff7b2b13b12c85f1ee
Mathias Jean Johansen <mathias@mjj.io>
2015-03-04 12:24:06 +0100

    Print stack instructions.

--- stack_machine/stack_machine.c
+++ stack_machine/stack_machine.c
@@ -4,6 +4,7 @@
 #include "utils.h"
 
 #define STACK_SIZE 1000
+#define LAST_INSTRUCTION -1
 
 #define PUSHGLOBAL 0
 #define PUSH 1
@@ -13,26 +14,64 @@
 #define SLIDE 5
 #define JUMP 6
 
-void print_instruction(int instruction) {
-  switch (instruction) {
-    case PUSHGLOBAL: printf("PUSHGLOBAL\n"); break;
-    case PUSH:       printf("PUSH\n"); break;
-    case PUSHINT:    printf("PUSHINT\n"); break;
-    case MKAP:       printf("MKAP\n"); break;
-    case UNWIND:     printf("UNWIND\n"); break;
-    case SLIDE:      printf("SLIDE\n"); break;
-    case JUMP:       printf("JUMP\n"); break;
-    default:         printf("<unknown>\n");
+void print_instructions(int* instructions) {
+  for (int i = 0; instructions[i] != LAST_INSTRUCTION; i++) {
+    switch (instructions[i]) {
+      case PUSHGLOBAL: { printf("PUSHGLOBAL %d; ", instructions[++i]); break; }
+      case PUSH: { printf("PUSH %d; ", instructions[++i]); break; }
+      case PUSHINT: { printf("PUSHINT %d; ", instructions[++i]); break; }
+      case MKAP: { printf("MKAP; "); break; }
+      case UNWIND: { printf("UNWIND; "); break; }
+      case SLIDE: { printf("SLIDE %d; ", instructions[++i]); break; }
+      case JUMP: { printf("JUMP %d; ", instructions[++i]); break; }
+      default: printf("<unknown> ");
+    }
   }
+  printf("\n");
 }
 
 int execute_instructions(int* program, int* stack) {
+  int sp = -1;
+  int pc = 0;
+
+  for (;;) {
+    switch (program[pc++]) {
+      case PUSHGLOBAL:
+        break;
+      case PUSH:
+        break;
+      case PUSHINT:
+        stack[sp + 1] = program[pc];
+        break;
+      case MKAP: {
+        int left = stack[sp];
+        int right = stack[sp-1];
+        // The stack needs to place the application node on top of the stack.
+        break;
+      }
+      case UNWIND:
+        break;
+      case SLIDE:
+        break;
+      case JUMP:
+        program[pc] = program[pc + 1];
+        pc++;
+        break;
+      default:
+        // printf("Illegal instruction %d at address %d\n", program[pc-1],
+        // pc-1);
+        exit(EXIT_FAILURE);
+    }
+  }
+
   return 1;
 }
 
 int execute(char* filename) {
   int* program = read_file(filename);
-  int* stack = (int*)malloc(sizeof(int)*STACK_SIZE);
+  int* stack = (int*)malloc(sizeof(int) * STACK_SIZE);
+
+  print_instructions(program);
 
   return execute_instructions(program, stack);
 }
--- stack_machine/utils.c
+++ stack_machine/utils.c
@@ -1,32 +1,21 @@
 #include "utils.h"
 
 int* read_file(char* filename) {
-  int capacity = 1, size = 0;
-  int* program = (int*)malloc(sizeof(int) * capacity);
-  FILE* source = fopen(filename, "r");
-  int instructions;
+  int* program = (int*)malloc(sizeof(int)*1000);
+  FILE *source = fopen(filename, "r");
+  int i = 0;
+  int num;
 
-  if (source == NULL) {
-    printf("File %s does not exist. Please, try again with an existing file.\n",
-           filename);
-    exit(EXIT_SUCCESS);
+  while (fscanf(source, "%d", &num) == 1) {
+    program[i] = num;
+    i++;
   }
 
-  while (fscanf(source, "%d", &instructions) == 1) {
-    if (size >= capacity) {
-      int* buffer = (int*)malloc(sizeof(int) * 2 * capacity);
-      int i;
-      for (i = 0; i < capacity; i++) {
-        buffer[i] = program[i];
-      }
-      free(program);
-      program = buffer;
-      capacity *= 2;
-    }
-    program[size++] = instructions;
-  }
+  // TODO: This needs to be fixed.
+  // Insert -1 at the end of array to determine where to stop.
+  program[i] = -1;
+
   fclose(source);
 
   return program;
 }
-
