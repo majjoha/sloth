88ca37183e92681509289dd35c048a5c40659aae
Mathias Jean Johansen <mathias@mjj.io>
2015-03-06 16:28:22 +0100

    Add functionality for compiling source files to .slo files.

--- compiler/slothc.ml
+++ compiler/slothc.ml
@@ -5,8 +5,6 @@ open Instructions
 open Compiler
 open Codegen
 
-exception Mismatch
-
 let from_string s = Parser.prog Lexer.token (Lexing.from_string s)
 
 let compile s = compProg (from_string s);;
@@ -16,10 +14,35 @@ let toFile (instructions:int list) (file:string) =
   let result =
     String.concat " " (List.map (fun i -> string_of_int(i)) instructions) in
   Printf.fprintf oc "%s\n" result;
-  close_out oc;;
-
-let example1 = 
-  codeGeneration (compile "main = f 2; f x = x")
+  close_out oc
 ;;
 
-let example2 = from_string "f f f f = x";;
+let readFile (file:string) =
+  let ic = open_in file in
+  let n = in_channel_length ic in
+  let s = Bytes.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  (s)
+;;
+
+let sourceToSlo (filename:string) (outfile:string) =
+  toFile (codeGeneration (compile (readFile filename))) outfile;
+  Printf.fprintf stdout "Succesfully compiled %s to %s.\n" filename outfile
+;;
+
+let () =
+  try
+    match (Array.length Sys.argv) with
+    | 2 -> let filename = Sys.argv.(1) in
+           let outname = (Filename.chop_extension (Sys.argv.(1)) ^ ".slo") in
+           sourceToSlo filename outname
+    | 3 -> let filename = Sys.argv.(1) in
+           let outname = Sys.argv.(2) in
+           sourceToSlo filename outname
+    | e -> prerr_string "Usage: slothc filename [outfile].\n";
+  with e ->
+    prerr_string "You need to pass a valid file path.\n";
+    prerr_string "Usage: slothc filename [outfile].\n";
+  exit 0
+;;
