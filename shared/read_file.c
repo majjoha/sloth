65a27fe93de19f8dc71cb7666c64967b30e2fe0a
Mathias Jean Johansen <mathias@mjj.io>
2015-04-08 11:34:39 +0200

    Extract read_file to its own file.

---
+++ shared/read_file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "read_file.h"
+
+int* read_file(char* filename) {
+  int* program = (int*)malloc(sizeof(int)*1000);
+  FILE *source = fopen(filename, "r");
+  int i = 0;
+  int num;
+
+  while (fscanf(source, "%d", &num) == 1) {
+    program[i] = num;
+    i++;
+  }
+
+  // Insert -1 at the end of array to determine where to stop.
+  program[i] = -1;
+
+  fclose(source);
+
+  return program;
+}
